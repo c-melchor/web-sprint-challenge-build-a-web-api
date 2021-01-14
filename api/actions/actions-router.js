e237f2cfc8d05f51e241e1c2174c65451858bb82
c_melchor <cah.melchor@gmail.com>
2021-01-14 14:20:44 -0800

    GET function is working, router set up for actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,15 @@
-// Write your "actions" router here!
+const express = require("express");
+const Actions = require("./actions-model");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Actions.get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "unable to retrieve actions" });
+    });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const actionsRouter = require("../api/actions/actions-router");
 const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
 server.use(express.json());
+server.use("/api/actions", actionsRouter);
 
 module.exports = server;
