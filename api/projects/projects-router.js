efa073d0fdd18dd23462df666a5be3d13e08b83a
c_melchor <cah.melchor@gmail.com>
2021-01-14 17:51:22 -0800

    PUT method working for projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -52,4 +52,14 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const editedProject = await Projects.update(id, req.body);
+    res.status(200).json(editedProject);
+  } catch (error) {
+    res.status(500).json({ errorMessage: "Unable to edit project" });
+  }
+});
+
 module.exports = router;
